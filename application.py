0108fcb57863bc34cb2519d10a6829fca28d05e5
Akhil Burle <akhilburle@gmail.com>
2020-06-28 22:04:23 -0400

    Update app file name

--- application.py
+++ application.py
@@ -3,17 +3,17 @@ from src.scrape import scrape
 import os
 
 # template_dir = os.path.abspath('../templates')
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def indexpage():
     return redirect(url_for("home"))
 
-@app.route("/home")
+@application.route("/home")
 def home():
     return render_template("index.html")
 
-@app.route("/getdata")
+@application.route("/getdata")
 def getData():
     ticker = request.args.get("ticker")
     scrape(ticker)
@@ -21,9 +21,9 @@ def getData():
     # print("ticker is: ", ticker)
     # return "ticker is: " + ticker
 
-@app.route("/download/<int:random>/<string:ticker>")
+@application.route("/download/<int:random>/<string:ticker>")
 def download(random, ticker):
     return send_file("deliverables/download.csv", as_attachment=True, attachment_filename=ticker+"BalanceSheet.csv", cache_timeout=0)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    application.run(port=5000, debug=True)
